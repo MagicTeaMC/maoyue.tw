218e97d72823e5f138322201b5e4dc2421a31640
MagicTeaMC <maoyuetw@proton.me>
2026-04-26 22:28:45 +0800

    use UTC+8

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,15 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
+function toTaiwanTime(date) {
+  return new Date(date.getTime() + 8 * 60 * 60 * 1000);
+}
+
 export async function GET(context) {
   const posts = await getCollection('blog');
-  const sortedPosts = posts.sort((a, b) => 
+  const sortedPosts = posts.sort((a, b) =>
     b.data.publishDate.getTime() - a.data.publishDate.getTime()
   );
-
   return rss({
     title: 'Maoyue Blog',
     description: 'Personal blog by Maoyue',
@@ -14,7 +17,7 @@ export async function GET(context) {
     items: sortedPosts.map((post) => ({
       title: post.data.title,
       description: post.data.description,
-      pubDate: post.data.publishDate,
+      pubDate: toTaiwanTime(post.data.publishDate),
       link: `/blog/${post.slug}/`,
       categories: post.data.tags || [],
     })),
